3b1098dacf002386fa0707366b3368c507bd5ddb
Tarrence van As <tarrence13@gmail.com>
2018-06-17 14:35:12 -0400

    Scaffold front-end

---
+++ packages/front-end/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Artonomous</h1>
+        </header>
+        <p className="App-intro">
+          A self-sustaining, self-improving, autonomous artist.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
