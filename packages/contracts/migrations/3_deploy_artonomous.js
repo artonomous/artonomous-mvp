540a6c9154c349460ccbac200da2c1d84f2bab22
Nick Reynolds <nicholas.s.reynolds@gmail.com>
2018-06-21 19:40:02 -0700

    adding unit tests and updating migrations/test deploy to include beneficiary

--- packages/contracts/migrations/3_deploy_artonomous.js
+++ packages/contracts/migrations/3_deploy_artonomous.js
@@ -1,6 +1,7 @@
 var ArtonomousStaking = artifacts.require("./ArtonomousStaking.sol");
 var Artonomous = artifacts.require("./Artonomous.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(Artonomous, ArtonomousStaking.address);
+module.exports = function(deployer, network, accounts) {
+  let beneficiary = accounts[0];
+  deployer.deploy(Artonomous, ArtonomousStaking.address, beneficiary);
 };
--- packages/contracts/test/claimArt.ts
+++ packages/contracts/test/claimArt.ts
@@ -7,34 +7,50 @@ const ArtonomousStaking = artifacts.require("ArtonomousStaking");
 configureChai(chai);
 const expect = chai.expect;
 
-contract ("Artonomous", accounts => {
-    let staking: any;
-    let artonomous: any;
+contract("Artonomous", accounts => {
+  let staking: any;
+  let artonomous: any;
 
-    beforeEach (async () => {
-        staking = await ArtonomousStaking.new("55f74ddc033dc34ca3b11ece54e6e569c24ea7da")
-        const stakingAddr = staking.address;
-        artonomous = await Artonomous.new(stakingAddr);
-    });
+  beforeEach(async () => {
+    staking = await ArtonomousStaking.new(
+      "55f74ddc033dc34ca3b11ece54e6e569c24ea7da"
+    );
+    const stakingAddr = staking.address;
+    artonomous = await Artonomous.new(stakingAddr, accounts[0]);
+  });
 
-    describe ("claimArt", () => {
-        it ("fails if other contract just deployed", async () => {
-            await expect(artonomous.claimArt()).to.eventually.be.rejectedWith("revert", "should not have allowed user to claim art immediately after contract deployed");
-        });
-        it ("succeeds if contract was deployed 24 hours ago", async () => {
-            await advanceEvmTime(86401); // just over 24 hours
-            await expect(artonomous.claimArt()).to.eventually.be.fulfilled("should have allowed user to claim art 24 hours after contract deployed");
-        });
-        it ("fails if previous auction was just ended (by claim) and new auction just began", async () => {
-            await advanceEvmTime(86401); // just over 24 hours
-            await expect(artonomous.claimArt()).to.eventually.be.fulfilled("should have allowed user to claim art 24 hours after contract deployed");
-            await expect(artonomous.claimArt()).to.eventually.be.rejectedWith("revert", "should not have allowed user to claim art immediately after previous auction ended (and new one started)");
-        });
-        it ("succeeds if new auction began 24 hours ago", async () => {
-            await advanceEvmTime(86401); // just over 24 hours
-            await expect(artonomous.claimArt()).to.eventually.be.fulfilled("should have allowed user to claim art 24 hours after contract deployed");
-            await advanceEvmTime(86401); // just over 24 hours
-            await expect(artonomous.claimArt()).to.eventually.be.fulfilled("should have allowed user to claim art 24 hours after initial auction finished and new one started");
-        });
+  describe("claimArt", () => {
+    it("fails if other contract just deployed", async () => {
+      await expect(artonomous.claimArt()).to.eventually.be.rejectedWith(
+        "revert",
+        "should not have allowed user to claim art immediately after contract deployed"
+      );
     });
+    it("succeeds if contract was deployed 24 hours ago", async () => {
+      await advanceEvmTime(86401); // just over 24 hours
+      await expect(artonomous.claimArt()).to.eventually.be.fulfilled(
+        "should have allowed user to claim art 24 hours after contract deployed"
+      );
+    });
+    it("fails if previous auction was just ended (by claim) and new auction just began", async () => {
+      await advanceEvmTime(86401); // just over 24 hours
+      await expect(artonomous.claimArt()).to.eventually.be.fulfilled(
+        "should have allowed user to claim art 24 hours after contract deployed"
+      );
+      await expect(artonomous.claimArt()).to.eventually.be.rejectedWith(
+        "revert",
+        "should not have allowed user to claim art immediately after previous auction ended (and new one started)"
+      );
+    });
+    it("succeeds if new auction began 24 hours ago", async () => {
+      await advanceEvmTime(86401); // just over 24 hours
+      await expect(artonomous.claimArt()).to.eventually.be.fulfilled(
+        "should have allowed user to claim art 24 hours after contract deployed"
+      );
+      await advanceEvmTime(86401); // just over 24 hours
+      await expect(artonomous.claimArt()).to.eventually.be.fulfilled(
+        "should have allowed user to claim art 24 hours after initial auction finished and new one started"
+      );
+    });
+  });
 });
