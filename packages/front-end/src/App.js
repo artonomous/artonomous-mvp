54aaa6e03d35cf8d85ac88342d9d4ba06c7c38ad
Nick Reynolds <nicholas.s.reynolds@gmail.com>
2018-06-22 13:52:23 -0700

    cleanup drizzle installation

--- packages/front-end/src/App.js
+++ packages/front-end/src/App.js
@@ -1,19 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-// Styles
-import './css/oswald.css'
-import './css/open-sans.css'
-import './css/pure-min.css'
-import './App.css'
+import "./App.css";
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        {this.props.children}
-      </div>
-    );
+    return <div className="App">{this.props.children}</div>;
   }
 }
 
-export default App
+export default App;
